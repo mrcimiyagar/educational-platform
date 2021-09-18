789731d2c1984151b76d7a783e6ec209b6fa82af
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-09-18 10:53:35 +0430

    Update serviceWorker.js

--- main/client-app/public/serviceWorker.js
+++ main/client-app/public/serviceWorker.js
@@ -1,9 +1,12 @@
+import { Icon } from "@material-ui/core";
+
 console.log("Service Worker Loaded...");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: data.body
+    body: data.body,
+    Icon: '/logo512.png'
   });
 });
