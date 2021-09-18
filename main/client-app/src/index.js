ee79b2bc00041b6662651970ed8fc3efe9697f55
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-09-18 09:06:01 +0430

    ...

--- main/client-app/src/index.js
+++ main/client-app/src/index.js
@@ -9,7 +9,7 @@ import store from './redux/main';
 import DesktopWallpaper from './images/desktop-wallpaper.webp';
 import DesktopWallpaper2 from './images/desktop-wallpaper.jpg';
 import { setup } from './util/Utils';
-import './notifier'
+import './notifications';
 
 export let pathConfig = {}
 
