6be3a37a67f29eb6cd42011905a82597bb4c7be1
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-09-18 09:28:36 +0430

    Update notifSystem.js

--- main/client-app/src/notifSystem.js
+++ main/client-app/src/notifSystem.js
@@ -1,4 +1,3 @@
-
 const publicVapidKey =
   'BNgD5u59pcsAJKNff5A8Wjw0sB-TKSmhfkXxLluZAB_ieQGTQdYDxG81EEsPMA_mzNN6GfWUS8XEMW6FOttCC8s'
 
@@ -12,61 +11,69 @@ async function send() {
   // Register Service Worker
   console.log('Registering service worker...')
 
-  navigator.serviceWorker.register('http://localhost:2002/serviceWorker.js', {scope: "/"})
-  .then(
-  function (reg) {
-      var serviceWorker;
-      if (reg.installing) {
-          serviceWorker = reg.installing;
+  navigator.serviceWorker
+    .register('http://localhost:2002/serviceWorker.js', { scope: '/' })
+    .then(
+      function (reg) {
+        var serviceWorker
+        let callback = async () => {
+          console.log('Service Worker Registered...')
+
+          // Register Push
+          console.log('Registering Push...')
+          const subscription = await reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+          })
+          console.log('Push Registered...')
+
+          // Send Push Notification
+          console.log('Sending Push...')
+          await fetch('https://backend.kaspersoft.cloud/subscribe', {
+            method: 'POST',
+            body: JSON.stringify(subscription),
+            headers: {
+              'content-type': 'application/json',
+            },
+          })
+          console.log('Push Sent...')
+        }
+        if (reg.installing) {
+          serviceWorker = reg.installing
           // console.log('Service worker installing');
-      } else if (reg.waiting) {
-          serviceWorker = reg.waiting;
+        } else if (reg.waiting) {
+          serviceWorker = reg.waiting
           // console.log('Service worker installed & waiting');
-      } else if (reg.active) {
-          serviceWorker = reg.active;
+        } else if (reg.active) {
+          serviceWorker = reg.active
           // console.log('Service worker active');
-      }
+        }
 
-      if (serviceWorker) {
-          console.log("sw current state", serviceWorker.state);
-          if (serviceWorker.state == "activated") {
-              //If push subscription wasnt done yet have to do here
-              console.log("sw already activated - Do watever needed here");
+        if (serviceWorker) {
+          console.log('sw current state', serviceWorker.state)
+          if (serviceWorker.state == 'activated') {
+            //If push subscription wasnt done yet have to do here
+            console.log('sw already activated - Do watever needed here')
+
+            callback();
           }
-          serviceWorker.addEventListener("statechange", async function(e) {
-              console.log("sw statechange : ", e.target.state);
-              if (e.target.state == "activated") {
-                  // use pushManger for subscribing here.
-                  console.log("Just now activated. now we can subscribe for push notification")
-                  
-  console.log('Service Worker Registered...')
+          serviceWorker.addEventListener('statechange', async function (e) {
+            console.log('sw statechange : ', e.target.state)
+            if (e.target.state == 'activated') {
+              // use pushManger for subscribing here.
+              console.log(
+                'Just now activated. now we can subscribe for push notification',
+              )
 
-  // Register Push
-  console.log('Registering Push...')
-  const subscription = await reg.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-  })
-  console.log('Push Registered...')
-
-  // Send Push Notification
-  console.log('Sending Push...')
-  await fetch('https://backend.kaspersoft.cloud/subscribe', {
-    method: 'POST',
-    body: JSON.stringify(subscription),
-    headers: {
-      'content-type': 'application/json',
-    },
-  })
-  console.log('Push Sent...')
-              }
-          });
-      }
-  },
-  function (err) {
-      console.error('unsuccessful registration with ', err);
-  }
-);
+              callback();
+            }
+          })
+        }
+      },
+      function (err) {
+        console.error('unsuccessful registration with ', err)
+      },
+    )
 }
 
 function urlBase64ToUint8Array(base64String) {
