6ba24148fca3bcafbd4712b20edecac2f0f572d9
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-09-22 11:27:13 +0330

    123

--- video-conference/public/js/screen.js
+++ video-conference/public/js/screen.js
@@ -168,7 +168,6 @@
           var remote_div = $('<div>')
           remote_div.attr('id', 'videoconf' + userId)
           remote_div[0].className = 'container'
-          remote_div[0].style.margin = '8px'
           remote_div[0].style.position = 'relative'
           $('body').append(remote_div)
           window.peer_media_elements[userId] = remote_div
