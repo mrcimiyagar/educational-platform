f12e35321c83865b8a1408acf436f96057165fc5
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-10-22 21:09:44 +0330

    ...

--- task-board/client/src/constants/Config.js
+++ task-board/client/src/constants/Config.js
@@ -1,6 +1,6 @@
 const SERVER_BASE_URL =
   process.env.REACT_APP_SERVER_BASE_URL ||
-  (process.env.NODE_ENV === 'production' ? '' : 'http://localhost:1337');
+  (process.env.NODE_ENV === 'production' ? '' : 'http://185.81.96.105:1337');
 
 const POSITION_GAP = 65535;
 
