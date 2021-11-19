1c3769ce589548d4d59083586dd69f571572eafc
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-11-19 22:57:55 +0330

    Update chat.js

--- main/client-app/src/routes/pages/chat.js
+++ main/client-app/src/routes/pages/chat.js
@@ -130,7 +130,7 @@ export default function Chat(props) {
       })
       .catch((error) => console.log('error', error))
 
-    return () => {if (goingToRoom) leaveRoom(() => {});}
+    return () => {if (!goingToRoom) leaveRoom(() => {});}
   }, [])
 
   registerDialogOpen(setOpen)
