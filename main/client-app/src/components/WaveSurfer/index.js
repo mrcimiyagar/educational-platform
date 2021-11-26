cac582dd756645e65a63e66b354e4d8ca6ad3443
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-11-26 11:22:15 +0330

    ...

--- main/client-app/src/components/WaveSurfer/index.js
+++ main/client-app/src/components/WaveSurfer/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { pathConfig } from "../..";
 import { token } from "../../util/settings";
+import { serverRoot } from "../../util/Utils";
 
 export function WaveSurferBox(props) {
   return (
     <iframe 
-        onLoad={() => {window.frames['wavesurfer-' + props.fileId].postMessage({sender: 'main', src: props.src, token: token, fileId: props.fileId}, pathConfig.waveSurferBox)}}
+        onLoad={() => {window.frames['wavesurfer-' + props.fileId].postMessage({sender: 'main', serverRoot: serverRoot, src: props.src, token: token, fileId: props.fileId}, pathConfig.waveSurferBox)}}
         id={'wavesurfer-' + props.fileId}
         name={'wavesurfer-' + props.fileId}
         frameBorder='0'
