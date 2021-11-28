b08bfbc2986f2dfebf492ae4be665ed9ec1ebb80
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-11-28 14:04:56 +0330

    Update videoPlayer.js

--- main/client-app/src/routes/pages/videoPlayer.js
+++ main/client-app/src/routes/pages/videoPlayer.js
@@ -21,21 +21,6 @@ export default function VideoPlayer(props) {
       setTimeout(popPage, 250);
   };
 
-  useEffect(() => {
-    if (open) {
-      setTimeout(() => {
-        let videoPlayer = document.getElementById('video-player')
-        videoPlayer.addEventListener('loadeddata', (e) => {
-          if (videoPlayer.readyState >= 3){
-            videoPlayer.style.width = window.innerWidth + 'px'
-            videoPlayer.style.height = (window.innerWidth * videoPlayer.videoHeight / videoPlayer.videoWidth) + 'px'
-            videoPlayer.play()
-          }
-        });
-      });
-    }
-  }, [open])
-
   return (
     <Dialog
             onTouchStart={(e) => {e.stopPropagation();}}
@@ -44,7 +29,7 @@ export default function VideoPlayer(props) {
           <IconButton style={{position: 'fixed', right: 12, top: 24}} onClick={() => {setOpen(false); setTimeout(popPage, 250)}}>
             <ArrowForwardTwoTone style={{fill: '#fff'}}/>
           </IconButton>
-          <video id={'video-player'} controls={true} style={{width: '100%', height: '100%', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}} 
+          <video id={'video-player'} controls={true} style={{width: '100%', maxWidth: 600, height: 'auto', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}} 
             src={props.src}/>
         </div>
       </Dialog>
