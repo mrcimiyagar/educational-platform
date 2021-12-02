04057427e4d87f7a730d0c2e453f599dd8a304d2
keyhan mohammadi <theprogrammermachine@gmail.com>
2021-12-02 12:07:23 +0330

    Update Login.jsx

--- planka/client/src/components/Login/Login.jsx
+++ planka/client/src/components/Login/Login.jsx
@@ -1,219 +1,53 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { useTranslation } from 'react-i18next';
-import { Form, Grid, Header, Message } from 'semantic-ui-react';
-import { useDidUpdate, usePrevious, useToggle } from '../../lib/hooks';
-import { Input } from '../../lib/custom-ui';
-import { useForm } from '../../hooks';
+import { Grid, Header } from 'semantic-ui-react';
 import styles from './Login.module.scss';
-
-const createMessage = (error) => {
-  if (!error) {
-    return error;
-  }
-
-  switch (error.message) {
-    case 'Invalid email or username':
-      return {
-        type: 'error',
-        content: 'common.invalidEmailOrUsername',
-      };
-    case 'Invalid password':
-      return {
-        type: 'error',
-        content: 'common.invalidPassword',
-      };
-    case 'Failed to fetch':
-      return {
-        type: 'warning',
-        content: 'common.noInternetConnection',
-      };
-    case 'Network request failed':
-      return {
-        type: 'warning',
-        content: 'common.serverConnectionFailed',
-      };
-    default:
-      return {
-        type: 'warning',
-        content: 'common.unknownError',
-      };
-  }
-};
 
 let username;
 let password;
 
-const Login = React.memo(
-  ({ defaultData, isSubmitting, error, onAuthenticate, onMessageDismiss }) => {
-    localStorage.removeItem('accessToken');
+const Login = React.memo(({ onAuthenticate }) => {
+  localStorage.removeItem('accessToken');
 
-    const [t] = useTranslation();
-    const wasSubmitting = usePrevious(isSubmitting);
+  const handleSubmit = useCallback(() => {
+    const cleanData = {};
 
-    const [data, handleFieldChange, setData] = useForm(() => ({
-      emailOrUsername: '',
-      password: '',
-      ...defaultData,
-    }));
+    cleanData.emailOrUsername = username;
+    cleanData.password = password;
 
-    const message = useMemo(() => createMessage(error), [error]);
-    const [focusPasswordFieldState, focusPasswordField] = useToggle();
+    onAuthenticate(cleanData);
+  }, [onAuthenticate]);
 
-    const emailOrUsernameField = useRef(null);
-    const passwordField = useRef(null);
-
-    const handleSubmit = useCallback(() => {
-      const cleanData = {
-        ...data,
-        emailOrUsername: data.emailOrUsername.trim(),
-      };
-
-      cleanData.emailOrUsername = username;
-      cleanData.password = password;
-
-      onAuthenticate(cleanData);
-    }, [onAuthenticate, data]);
-
-    useEffect(() => {
-      emailOrUsernameField.current.select();
-    }, []);
-
-    useEffect(() => {
-      if (wasSubmitting && !isSubmitting && error) {
-        switch (error.message) {
-          case 'Invalid email or username':
-            emailOrUsernameField.current.select();
-
-            break;
-          case 'Invalid password':
-            setData((prevData) => ({
-              ...prevData,
-              password: '',
-            }));
-            focusPasswordField();
-
-            break;
-          default:
-        }
+  window.onmessage = (e) => {
+    if (e.data.sender === 'main') {
+      if (e.data.action === 'init') {
+        username = e.data.username;
+        password = e.data.password;
+        handleSubmit();
       }
-    }, [isSubmitting, wasSubmitting, error, setData, focusPasswordField]);
+    }
+  };
 
-    useDidUpdate(() => {
-      passwordField.current.focus();
-    }, [focusPasswordFieldState]);
-
-    window.onmessage = (e) => {
-      if (e.data.sender === 'main') {
-        if (e.data.action === 'init') {
-          username = e.data.username;
-          password = e.data.password;
-          handleSubmit();
-        }
-      }
-    };
-
-    return (
-      <div className={classNames(styles.wrapper, styles.fullHeight)}>
-        <Grid verticalAlign="middle" className={styles.fullHeightPaddingFix}>
-          <Grid.Column widescreen={4} largeScreen={5} computer={6} tablet={16} mobile={16}>
-            <Grid verticalAlign="middle" className={styles.fullHeightPaddingFix}>
-              <Grid.Column>
-                <div className={styles.loginWrapper}>
-                  <Header
-                    as="h1"
-                    textAlign="center"
-                    content={t('common.logInToPlanka')}
-                    className={styles.formTitle}
-                  />
-                  <div>
-                    {message && (
-                      <Message
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...{
-                          [message.type]: true,
-                        }}
-                        visible
-                        content={t(message.content)}
-                        onDismiss={onMessageDismiss}
-                      />
-                    )}
-                    <Form size="large" onSubmit={handleSubmit}>
-                      <div className={styles.inputWrapper}>
-                        <div className={styles.inputLabel}>{t('common.emailOrUsername')}</div>
-                        <Input
-                          fluid
-                          ref={emailOrUsernameField}
-                          name="emailOrUsername"
-                          value={data.emailOrUsername}
-                          readOnly={isSubmitting}
-                          className={styles.input}
-                          onChange={handleFieldChange}
-                        />
-                      </div>
-                      <div className={styles.inputWrapper}>
-                        <div className={styles.inputLabel}>{t('common.password')}</div>
-                        <Input.Password
-                          fluid
-                          ref={passwordField}
-                          name="password"
-                          value={data.password}
-                          readOnly={isSubmitting}
-                          className={styles.input}
-                          onChange={handleFieldChange}
-                        />
-                      </div>
-                      <Form.Button
-                        primary
-                        size="large"
-                        icon="right arrow"
-                        labelPosition="right"
-                        content={t('action.logIn')}
-                        floated="right"
-                        loading={isSubmitting}
-                        disabled={isSubmitting}
-                      />
-                    </Form>
-                  </div>
-                </div>
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column
-            widescreen={12}
-            largeScreen={11}
-            computer={10}
-            only="computer"
-            className={classNames(styles.cover, styles.fullHeight)}
-          >
-            <div className={styles.descriptionWrapperOverlay} />
-            <div className={styles.descriptionWrapper}>
-              <Header inverted as="h1" content="Planka" className={styles.descriptionTitle} />
-              <Header
-                inverted
-                as="h2"
-                content={t('common.projectManagement')}
-                className={styles.descriptionSubtitle}
-              />
-            </div>
-          </Grid.Column>
-        </Grid>
-      </div>
-    );
-  },
-);
+  return (
+    <div className={classNames(styles.wrapper, styles.fullHeight)}>
+      <Grid verticalAlign="middle" className={styles.fullHeightPaddingFix}>
+        <Grid.Column widescreen={4} largeScreen={5} computer={6} tablet={16} mobile={16}>
+          <Grid verticalAlign="middle" className={styles.fullHeightPaddingFix}>
+            <Grid.Column>
+              <div className={styles.loginWrapper}>
+                <Header as="h1" textAlign="center" content="" className={styles.formTitle} />
+              </div>
+            </Grid.Column>
+          </Grid>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+});
 
 Login.propTypes = {
-  defaultData: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  isSubmitting: PropTypes.bool.isRequired,
-  error: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   onAuthenticate: PropTypes.func.isRequired,
-  onMessageDismiss: PropTypes.func.isRequired,
-};
-
-Login.defaultProps = {
-  error: undefined,
 };
 
 export default Login;
