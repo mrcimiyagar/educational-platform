461d26028204eee6cbbe58adf390c22e329d0bb5
edris <theprogrammermachine@gmail.com>
2021-12-16 23:22:37 +0330

    changed backcolor

--- main/client-app/src/components/HomeMessenger/index.js
+++ main/client-app/src/components/HomeMessenger/index.js
@@ -83,7 +83,7 @@ export default function HomeMessenger(props) {
     useEffect(() => {
         setWallpaper({
           type: 'color',
-          color: colors.primaryMedium
+          color: colors.accentDark
         });
     }, []);
     return <div
