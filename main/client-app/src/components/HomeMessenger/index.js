940072c4d015c1728c3a613a0da8f1a374eb74b0
edris <theprogrammermachine@gmail.com>
2021-12-17 09:27:55 +0330

    test

--- main/client-app/src/components/HomeMessenger/index.js
+++ main/client-app/src/components/HomeMessenger/index.js
@@ -151,8 +151,8 @@ export default function HomeMessenger(props) {
                 : isTablet()
                 ? 'calc(100% - 168px)'
                 : 'calc(100% - 128px - 32px)',
-              backgroundColor: !(isMobile() || isTablet()) ? 'rgba(255, 255, 255, 0.445)' : undefined,
-              backdropFilter: !(isMobile() || isTablet()) ? 'blur(10px)' : undefined,
+              backgroundColor: (isDesktop() || isTablet()) ? 'rgba(255, 255, 255, 0.445)' : undefined,
+              backdropFilter: (isDesktop() || isTablet()) ? 'blur(10px)' : undefined,
               marginLeft: isMobile() ? -8 : undefined,
               marginRight: isMobile() || isDesktop() ? -8 : undefined,
               marginTop: isDesktop() ? 88 : 56,
