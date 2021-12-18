3505e1a608f764fae5d03c994df29f4122cfdcd0
edris <theprogrammermachine@gmail.com>
2021-12-18 21:07:45 +0330

    test

--- micro-router/server.js
+++ micro-router/server.js
@@ -3,66 +3,20 @@ let httpProxy = require('http-proxy')
 let https = require('https')
 let fs = require('fs')
 
-var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
-
-var getHostPortFromString = function (hostString, defaultPort) {
-  var host = hostString;
-  var port = defaultPort;
-
-  var result = regex_hostport.exec(hostString);
-  if (result != null) {
-    host = result[1];
-    if (result[2] != null) {
-      port = result[3];
-    }
-  }
-
-  return ( [host, port] );
-};
-
-var proxy = httpProxy.createProxyServer({changeOrigin: true, ws: true});
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+  ws: true,
+  ssl: {
+    key: fs.readFileSync('server.key', 'utf8'),
+    cert: fs.readFileSync('server.cert', 'utf8')
+  },
+  secure: true
+});
 
 let serv = https.createServer({
   cert: fs.readFileSync('server.cert'),
   key: fs.readFileSync('server.key')
 }, function(req, res) {
-
-  var hostPort = getHostPortFromString(req.url, 443);
-  var hostDomain = hostPort[0];
-  var port = parseInt(hostPort[1]);
-  console.log("Proxying HTTPS request for:", hostDomain, port);
-
-  var proxySocket = req.socket;
-  proxySocket.connect(port, hostDomain, function () {
-      //proxySocket.write(bodyhead);
-      socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
-    }
-  );
-
-  proxySocket.on('data', function (chunk) {
-    socket.write(chunk);
-  });
-
-  proxySocket.on('end', function () {
-    socket.end();
-  });
-
-  proxySocket.on('error', function () {
-    socket.write("HTTP/" + req.httpVersion + " 500 Connection error\r\n\r\n");
-    socket.end();
-  });
-
-  socket.on('data', function (chunk) {
-    proxySocket.write(chunk);
-  });
-
-  socket.on('end', function () {
-    proxySocket.end();
-  });
-
-  socket.on('error', function () {
-    proxySocket.end();
-  });
 
   if (req.headers.host === 'kaspersoft.cloud') {
     proxy.web(req, res, { target: 'http://localhost:2001' }, e => {})
