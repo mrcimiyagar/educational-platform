fe80eec732eecb2598da1e9b839b647da8dccfed
edris <theprogrammermachine@gmail.com>
2021-12-18 20:02:45 +0330

    test

--- micro-router/server.js
+++ micro-router/server.js
@@ -82,6 +82,61 @@ serv.on('upgrade', function (req, socket, head) {
 
 serv.listen(443)
 
+var getHostPortFromString = function (hostString, defaultPort) {
+  var host = hostString;
+  var port = defaultPort;
+
+  var result = regex_hostport.exec(hostString);
+  if (result != null) {
+    host = result[1];
+    if (result[2] != null) {
+      port = result[3];
+    }
+  }
+
+  return ( [host, port] );
+};
+
+serv.addListener('connect', function (req, socket, bodyhead) {
+  var hostPort = getHostPortFromString(req.url, 443);
+  var hostDomain = hostPort[0];
+  var port = parseInt(hostPort[1]);
+  console.log("Proxying HTTPS request for:", hostDomain, port);
+
+  var proxySocket = new net.Socket();
+  proxySocket.connect(port, hostDomain, function () {
+      proxySocket.write(bodyhead);
+      socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
+    }
+  );
+
+  proxySocket.on('data', function (chunk) {
+    socket.write(chunk);
+  });
+
+  proxySocket.on('end', function () {
+    socket.end();
+  });
+
+  proxySocket.on('error', function () {
+    socket.write("HTTP/" + req.httpVersion + " 500 Connection error\r\n\r\n");
+    socket.end();
+  });
+
+  socket.on('data', function (chunk) {
+    proxySocket.write(chunk);
+  });
+
+  socket.on('end', function () {
+    proxySocket.end();
+  });
+
+  socket.on('error', function () {
+    proxySocket.end();
+  });
+  
+});
+
 setInterval(() => {
   const used = process.memoryUsage().heapUsed / 1024 / 1024;
   console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
