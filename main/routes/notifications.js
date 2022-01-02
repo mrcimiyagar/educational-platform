f35ed0f3e8ffa1727448dc1a33e42bdb34b50166
edris <theprogrammermachine@gmail.com>
2022-01-02 18:13:33 +0330

    .

--- main/routes/notifications.js
+++ main/routes/notifications.js
@@ -16,15 +16,21 @@ router.post('/sync', jsonParser, async function (req, res) {
     });
 });
 
-router.post('/recycle', jsonParser, async function (req, res) {
-    authenticateMember(req, res, async (membership, session, user, acc) => {
-        if (notifs[user.id] === undefined) {
-            res.send({status: 'success'});
-            return;
-        }
-        notifs[user.id].splice(0, req.body.notifsCount);
+router.post('/sync-bot', jsonParser, async function (req, res) {
+    let session = await sw.Session.findOne({where: {token: req.headers.token}});
+    let notifsCount = notifs[session.userId] === undefined ? 0 : notifs[session.userId].length;
+    let nots = notifs[session.userId].slice(0, notifsCount);
+    res.send({status: 'success', notifications: nots});
+});
+
+router.post('/recycle-bot', jsonParser, async function (req, res) {
+    let session = await sw.Session.findOne({where: {token: req.headers.token}});
+    if (notifs[session.userId] === undefined) {
         res.send({status: 'success'});
-    });
+        return;
+    }
+    notifs[session.userId].splice(0, req.body.notifsCount);
+    res.send({status: 'success'});
 });
 
 module.exports = router;
