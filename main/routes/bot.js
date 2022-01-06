3c3d10fdf0f4b8764d7ccd0cdc37b1b8f9a425a6
edris <theprogrammermachine@gmail.com>
2022-01-06 10:04:49 +0330

    .

--- main/routes/bot.js
+++ main/routes/bot.js
@@ -1074,7 +1074,7 @@ router.post('/notify_gui_base_activated', jsonParser, async function (req, res) 
       if (req.body.preview === false) {
         let widgetWorker = undefined;
         if (membership !== undefined && membership !== null) {
-          widgetWorker = await sw.WidgetWorker.findOne({where: {widgetId: widget.id, roomId: membership.roomId}});
+          widgetWorker = await sw.WidgetWorker.findOne({where: {id: req.body.widgetWorkerId, roomId: membership.roomId}});
           widget = await sw.Widget.findOne({where: {id: widgetWorker.widgetId}});
         }
       }
