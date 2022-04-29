f99064c21bf5a7f3870faa7d8b6e552f991807d0
edris <theprogrammermachine@gmail.com>
2022-04-29 13:00:46 +0430

    .

--- main/client-app/public/serviceWorker.js
+++ main/client-app/public/serviceWorker.js
@@ -1,55 +1,75 @@
-
 console.log("Service Worker Loaded...");
 
-let cacheName = 'js13kPWA-v2';
+let cacheName = "js13kPWA-v2";
 
-self.addEventListener("push", e => {
+self.addEventListener("push", (e) => {
   const data = e.data.json();
   console.log("Push Recieved...");
-  let notif = new Notification("Society", {
+  self.registration.showNotification("Society", {
     body: data.body,
-    icon: '/logo512.png'
+    icon: "/logo512.png",
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    actions: [
+      {
+        action: 'openChat',
+        title: 'Open chat'
+      }
+    ]
   });
-  notif.onclick = function(){
+  notif.onclick = function () {
     window.parent.focus();
     notif.close();
-   }
+  };
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key === cacheName) { return; }
-      return caches.delete(key);
-    }))
-  }));
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  if (event.action === 'openChat') {
+    self.clients.openWindow('/');
+  }
+}, false);
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
 });
 
 const KEY = cacheName;
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'CACHE_URLS') {
-        event.waitUntil(
-            caches.open(KEY)
-                .then( (cache) => {
-                    return cache.addAll(event.data.payload);
-                })
-        );
-    }
+self.addEventListener("message", (event) => {
+  if (event.data.type === "CACHE_URLS") {
+    event.waitUntil(
+      caches.open(KEY).then((cache) => {
+        return cache.addAll(event.data.payload);
+      })
+    );
+  }
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    try {
-    const response = await fetch(e.request);
-    return response;
-    } catch(ex) {
-      console.error(ex);
-    }
-    return null;
-  })());
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      try {
+        const response = await fetch(e.request);
+        return response;
+      } catch (ex) {
+        console.error(ex);
+      }
+      return null;
+    })()
+  );
 });
