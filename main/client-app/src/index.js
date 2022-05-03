38fe1b4e80d3c8db6cd4f74bae7cd2c90768557d
edris <theprogrammermachine@gmail.com>
2022-05-03 20:20:25 +0430

    .

--- main/client-app/src/index.js
+++ main/client-app/src/index.js
@@ -13,30 +13,6 @@ import SpaceWallpaperDark from "./images/space-wallpaper-dark.png";
 import SpaceWallpaperLight from "./images/space-wallpaper-light.jpg";
 import { ColorBase, colors, setThemeMode, themeMode } from "./util/settings";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging, onMessage, getToken } from "firebase/messaging";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDGWqgvpAwwwPk1ibxWkF0b80dt8NQllFs",
-  authDomain: "infinity-e17df.firebaseapp.com",
-  projectId: "infinity-e17df",
-  storageBucket: "infinity-e17df.appspot.com",
-  messagingSenderId: "538387159430",
-  appId: "1:538387159430:web:bd74cea2daa5973a947407",
-  measurementId: "G-2PH313X4HV",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const messaging = getMessaging(app);
-
 const channel4Broadcast = new BroadcastChannel("channel4");
 channel4Broadcast.onmessage = (event) => {
   channel4Broadcast.postMessage({ token: localStorage.getItem("token") });
@@ -227,57 +203,6 @@ let AppContainer = (props) => {
           .then((result) => {
             pathConfig = result;
             setup();
-            getToken(messaging, {
-              vapidKey:
-                "BDztmrHz8czoaLGG8WgOnWk7FX2z15TYZpgyDxzZQrcVF8tnNJwTS_kIn_JZAbQ-ZrLmpGafELrz2xPgOsonT9k",
-            })
-              .then((currentToken) => {
-                /*if (currentToken) {
-                  onMessage(messaging, (payload) => {
-                    alert('hello');
-                    console.log(
-                      "[firebase-messaging-sw.js] Received background message ",
-                      payload
-                    );
-                    const notificationTitle = payload.notification.title;
-                    const notificationOptions = {
-                      body: payload.notification.body,
-                      icon: "/logo512.png",
-                      vibrate: [200, 100, 200, 100, 200, 100, 200],
-                    };
-                    let notification = new Notification(
-                      notificationTitle,
-                      notificationOptions
-                    );
-                    notification.onclick = () => {
-                      notification.close();
-                    };
-                  });
-                  let requestOptions = {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                      token: localStorage.getItem("token"),
-                    },
-                    body: JSON.stringify({
-                      firebaseToken: currentToken,
-                    }),
-                    redirect: "follow",
-                  };
-                  fetch(serverRoot + "/registerFirebaseToken", requestOptions)
-                    .then((response) => response.json())
-                    .then((result) => {
-                      console.log(JSON.stringify(result));
-                    });
-                } else {
-                  console.log(
-                    "No registration token available. Request permission to generate one."
-                  );
-                }*/
-              })
-              .catch((err) => {
-                console.log("An error occurred while retrieving token. ", err);
-              });
             loaded = true;
             setTimeout(() => {
               setDisplay("none");
--- main/client-app/src/notifSystem.js
+++ main/client-app/src/notifSystem.js
@@ -1,3 +1,28 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, onMessage, getToken } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGWqgvpAwwwPk1ibxWkF0b80dt8NQllFs",
+  authDomain: "infinity-e17df.firebaseapp.com",
+  projectId: "infinity-e17df",
+  storageBucket: "infinity-e17df.appspot.com",
+  messagingSenderId: "538387159430",
+  appId: "1:538387159430:web:bd74cea2daa5973a947407",
+  measurementId: "G-2PH313X4HV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
 const publicVapidKey =
   'BNgD5u59pcsAJKNff5A8Wjw0sB-TKSmhfkXxLluZAB_ieQGTQdYDxG81EEsPMA_mzNN6GfWUS8XEMW6FOttCC8s'
 
@@ -19,12 +44,70 @@ async function send() {
         let callback = async () => {
           console.log('Service Worker Registered...');
 
-          // Register Push
-          console.log('Registering Push...')
-          const subscription = await reg.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-          })
+        let requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          redirect: "follow",
+        };
+        fetch("https://config.kasperian.cloud", requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            setup();
+            getToken(messaging, {
+              vapidKey:
+                "BDztmrHz8czoaLGG8WgOnWk7FX2z15TYZpgyDxzZQrcVF8tnNJwTS_kIn_JZAbQ-ZrLmpGafELrz2xPgOsonT9k",
+            })
+              .then((currentToken) => {
+                if (currentToken) {
+                  onMessage(messaging, (payload) => {
+                    alert('hello');
+                    console.log(
+                      "[firebase-messaging-sw.js] Received background message ",
+                      payload
+                    );
+                    const notificationTitle = payload.notification.title;
+                    const notificationOptions = {
+                      body: payload.notification.body,
+                      icon: "/logo512.png",
+                      vibrate: [200, 100, 200, 100, 200, 100, 200],
+                    };
+                    Notification.requestPermission(function(result) {
+                      if (result === 'granted') {
+                        navigator.serviceWorker.ready.then(function(registration) {
+                          registration.showNotification(notificationTitle, notificationOptions);
+                        });
+                      }
+                    });
+                  });
+                  let requestOptions = {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      token: localStorage.getItem("token"),
+                    },
+                    body: JSON.stringify({
+                      firebaseToken: currentToken,
+                    }),
+                    redirect: "follow",
+                  };
+                  fetch(serverRoot + "/registerFirebaseToken", requestOptions)
+                    .then((response) => response.json())
+                    .then((result) => {
+                      console.log(JSON.stringify(result));
+                    });
+                } else {
+                  console.log(
+                    "No registration token available. Request permission to generate one."
+                  );
+                }
+              })
+              .catch((err) => {
+                console.log("An error occurred while retrieving token. ", err);
+              });
+          });
+
           console.log('Push Registered...')
 
           // Send Push Notification
